1eb10be6206c813ec0478ec6ef1b64cc4b567f88
Sprinkle15 <77218471+Sprinkle15@users.noreply.github.com>
2021-01-09 20:56:07 -0600

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5ffa68cbacd62200175d2936",
+				"5ffa68cbacd62200175d2938"
+			]
+		},
+		"5ffa68cbacd62200175d2936": {
+			"id": "5ffa68cbacd62200175d2936",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5ffa68cbacd62200175d2938": {
+			"id": "5ffa68cbacd62200175d2938",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
